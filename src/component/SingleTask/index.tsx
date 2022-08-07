b60ac3af47be8d8947a3db512453c90f38448833
ErikYSH <yangerik1113@gmail.com>
2022-08-06 22:08:56 -0700

    checkbox and delete function added

--- src/component/SingleTask/index.tsx
+++ src/component/SingleTask/index.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
-import { Task } from '../Task';
+import React from "react";
+import { Task } from "../Task";
+import { RiEditBoxLine, RiDeleteBin4Line } from "react-icons/ri";
+import { GrStatusGood, GrTask } from "react-icons/gr";
 
 interface Props {
-    task: Task
-    taskList: Task[]
-    setTaskList: React.Dispatch<React.SetStateAction<Task[]>>
+  task: Task;
+  taskList: Task[];
+  setTaskList: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
-const SingleTask = ({task, taskList, setTaskList}:Props) => {
-    return(
-        <div>
-            <form className='singleTodo'>
-                <span className='singleTaskText'> {task.task}</span>
-            </form>
-        </div>
-    )
-}
+const SingleTask = ({ task, taskList, setTaskList }: Props) => {
+  const handleComplete = (id: number) => {
+    setTaskList(
+      taskList.map((task) =>
+        task.id === id ? { ...task, isComplete: !task.isComplete } : task
+      )
+    );
+  };
+
+  const handleDelete = (id:number) => {
+    setTaskList(taskList.filter((task) => task.id !== id))
+  }
+
+  return (
+    <div>
+      <form className="singleTodo">
+        {task.isComplete ? <s className='singleTaskText'>{task.task}</s> : <span className='singleTaskText'>{task.task}</span>}
+        <span className="icons">
+          <RiEditBoxLine />
+        </span>
+        <span className="icons" onClick={() => handleDelete(task.id)}>
+          <RiDeleteBin4Line />
+        </span>
+        <span className="icons" onClick={() => handleComplete(task.id)}>
+          <GrStatusGood />
+        </span>
+      </form>
+    </div>
+  );
+};
 
 export default SingleTask;
