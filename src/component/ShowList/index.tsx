6be0767b37991bddda5c184db9abebdca89c7d08
ErikYSH <yangerik1113@gmail.com>
2022-08-07 23:35:54 -0700

    added search term

--- src/component/ShowList/index.tsx
+++ src/component/ShowList/index.tsx
@@ -1,29 +1,43 @@
-import React from 'react'
-import {Task} from '../Task'
-import SingleTask from "../SingleTask"
-import "./index.css"
+import React, { useState } from "react";
+import { Task } from "../Task";
+import SingleTask from "../SingleTask";
+import "./index.css";
 
 interface Props {
-    taskList: Task[];
-    setTaskList: React.Dispatch<React.SetStateAction<Task[]>>
+  taskList: Task[];
+  setTaskList: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
-const ShowList:React.FC<Props> = ({taskList, setTaskList}:Props) => {
-    return (
-        <div className='showList'>
-            {
-                taskList.map((task) => (
-                    <SingleTask 
-                        task={task}
-                        // id = {task.id}
-                        taskList = {taskList}
-                        setTaskList = {setTaskList}
-                    />
-                ))
-            }
-        </div>
-    )
+const ShowList: React.FC<Props> = ({ taskList, setTaskList }: Props) => {
+  const [search, setSearch] = useState("");
 
-}
+  return (
+    <div className="showList">
+      <input
+        type="text"
+        placeholder="Search Task"
+        className="search"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+
+      {taskList.filter((task)=> {
+        if (search === "") {
+            return task
+        } else if (task.task.toLowerCase().includes(search.toLowerCase())) {
+            return task
+        }
+      }).map((task) => (
+        <SingleTask
+          task={task}
+          // id = {task.id}
+          taskList={taskList}
+          setTaskList={setTaskList}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ShowList;
